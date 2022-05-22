f6bdbd876044b91ad3824f4d941b8d64c9694cb3
Thierry <marcelinthierry@gmail.com>
2022-05-22 16:17:03 +0800

    Create a new registration page

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -6,6 +6,7 @@ require_relative './app'
 module EtaShare
   # Web controller for Credence API
   class App < Roda
+    plugin :public, root: 'app/presentation/public'
     route('auth') do |routing|
       @login_route = '/auth/login'
       routing.is 'login' do
